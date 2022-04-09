b188bb172974dcaa70c3759b84b0caf12f54f1ec
tunnelview <86519065+tunnelview@users.noreply.github.com>
2022-04-09 15:24:31 +1000

    Fix closing tags erros in CustomCard.js

--- src/components/card/CustomCard.js
+++ src/components/card/CustomCard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, Button} from "react-bootstrap";
+import { Card, Button } from "react-bootstrap";
 
 export const CustomCard = () => {
   return (
@@ -7,12 +7,12 @@ export const CustomCard = () => {
       <Card.Img variant="top" src="holder.js/100px180" />
       <Card.Body>
         <Card.Title>Card Title</Card.Title>
+        <Card.Title>Rating : 4.4</Card.Title>
 
         <div className="d-flex.justify-content-around">
-
-        <Button variant="Info">Go somewhere</Button>
-        <Button variant="Secondary">Go somewhere</Button>
-        <div />
+          <Button variant="info"> Be Happy</Button>
+          <Button variant="secondary"> Lazy Bee </Button>
+        </div>
       </Card.Body>
     </Card>
   );
--- src/components/search-form/SearchForm.js
+++ src/components/search-form/SearchForm.js
@@ -1,13 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { Col, Row, Form, Button } from "react-bootstrap";
 
 export const SearchForm = () => {
+  const [search, setSearch] = useState("");
+
+  const handleOnChange = (e) => {
+    const { value } = e.target;
+    setSearch(value);
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    console.log(search);
+  };
+
   return (
     <div>
-      <Form>
+      <Form onSubmit={handleOnSubmit}>
         <Row>
           <Col>
-            <Form.Control placeholder="First name" />
+            <Form.Control placeholder="First name" onChange={handleOnChange} />
           </Col>
           <Col>
             <Button variant="warning" type="submit">
